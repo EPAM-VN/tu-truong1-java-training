06e2a214caecfff5d5d0bbd365ef4b97f05eb99d
Tu Truong <tu_truong1@epam.com>
2026-08-19 22:08:09 +0700

    add order endpoints

---
+++ pet-order/order.web/src/main/java/local/jt/pet/order/web/configurations/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package local.jt.pet.order.web.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/", "/index", "/index.html", "/favicon.ico", "/api/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin((form) -> {
+                form
+                    .loginPage("/login")
+                    .permitAll();
+            })
+            .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("tutruong")
+                        .password("1qaZ2wsX@1234")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
---
+++ pet-order/order.web/src/main/java/local/jt/pet/order/web/controllers/orders/OrderController.java
@@ -0,0 +1,17 @@
+package local.jt.pet.order.web.controllers.orders;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/orders")
+public class OrderController {
+    @GetMapping
+    public List<String> getOrders() {
+        return Arrays.asList(new String[] {"1", "2", "3"});
+    }
+}
